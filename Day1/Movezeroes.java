aa841d9cc7f4349063f5c11b57d3d9ef0d0f989f
rikitadsouza <rikitadsouza06@gmail.com>
2024-07-14 20:18:23 +0530

    problem 2

---
+++ Day1/Movezeroes.java
@@ -0,0 +1,39 @@
+package Day1;
+
+public class Movezeroes {
+    public void Movezeroes(int[] nums) {
+        int nonZeroIndex = 0;
+        
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
+            }
+        }
+        
+        
+        for (int i = nonZeroIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Movezeroes solution = new Movezeroes();
+        int[] nums = {0, 1, 0, 3, 12};
+        
+        System.out.println("Before moving zeroes:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        solution.Movezeroes(nums);
+
+        System.out.println("After moving zeroes:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
